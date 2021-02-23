dd52b886d085e0f55d49a5c42c53d524d5d41c8a
ShadowEagle56 <79435543+ShadowEagle56@users.noreply.github.com>
2021-02-23 17:22:27 +0800

    Update FindBestDice.py

--- FindBestDice.py
+++ FindBestDice.py
@@ -1,4 +1,4 @@
-def count_wins(dice1, dice2):
+def count_wins(dice1, dice2):                       # Create a function to find how many times each dice wins.
     assert len(dice1) == 6 and len(dice2) == 6
     dice1_wins, dice2_wins = 0, 0
     for x in dice1:
@@ -7,13 +7,13 @@ def count_wins(dice1, dice2):
                 dice1_wins += 1
             elif x < y:
                 dice2_wins += 1
-    return dice1_wins, dice2_wins
+    return dice1_wins, dice2_wins                  # Return the value of wins for each dice.
 
 
-def find_the_best_dice(dices):
+def find_the_best_dice(dices):                     # Create a function to find which dice wins the most.
     assert all(len(dice) == 6 for dice in dices)
-    wins = [0]*len(dices)
-    for x in range(len(dices)):
+    wins = [0]*len(dices)                          # Create an empty array with spaces equivalent to the number of dices.
+    for x in range(len(dices)):                    # Compare 2 dices and store the number of times each dice wins into the array wins[].
         for y in range(x + 1, len(dices)):
             a, b = count_wins(dices[x], dices[y])
             if a > b:
@@ -22,12 +22,12 @@ def find_the_best_dice(dices):
                 wins[y] += 1
     check = True
     elem = wins[0]
-    for x in wins:
+    for x in wins:                                 # Check if there is a duplicate number of wins for each dice.
         if elem != wins[x]:
             check = False
 
     if check is True:
-        return -1
+        return -1                                  # Returns -1 if there is no best dice
     elif check is False:
         max_int = max(wins)
         max_index = wins.index(max_int)
@@ -39,18 +39,18 @@ def find_the_best_dice(dices):
             if wins[max_index] == wins[x]:
                 check = True
         if check is True:
-            return -1
+            return -1                              # Returns -1 if there is no best dice
         else:
-            return max_index
+            return max_index                       # Return the dice which wins the most
 
 
-def compute_strategy(dices):
+def compute_strategy(dices):                       # Create a function that finds the best dice which is most likely to win against another dice.
     assert all(len(dice) == 6 for dice in dices)
 
     strategy = dict()
-    strategy["choose_first"] = True
+    strategy["choose_first"] = True                # Determine whether the user is going first or second.
 
-    if find_the_best_dice(dices) != -1:
+    if find_the_best_dice(dices) != -1:            # Make sure that there is a best dice among the dices listed.
         strategy["choose_first"] = True
         strategy["first_dice"] = find_the_best_dice(dices)
     else:
@@ -62,7 +62,4 @@ def compute_strategy(dices):
                     break
             strategy[x] = y
 
-    print(strategy)
-
-
-compute_strategy(dices)
+    return strategy                                # Return the best dice that the user can choose to win.
